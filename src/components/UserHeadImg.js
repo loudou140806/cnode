707823cbee2557647d210305d0f5472a3e4f33c0
loudou140806 <546587684@qq.com>
2017-07-13 00:56:30 +0800

    dist

--- src/components/UserHeadImg.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class UserHeadImg extends Component {
-    render() {
-        return (<div className="user-headimg" style={{ backgroundImage: 'url(' + this.props.url + ')' }}></div>)
-    }
-}
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TabIcon from './TabIcon';
-import UserHeadImg from './UserHeadImg';
-import Loading from './Loading';
-import TipMsgSignin from './TipMsgSignIn';
-import Header from './Header';
-import NoData from './NoData';
-
-export { TabIcon, UserHeadImg, Loading, TipMsgSignin, Header, NoData };
